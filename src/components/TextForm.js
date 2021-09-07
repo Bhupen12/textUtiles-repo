c8d8483d58ced6f24c805985d7ada1ac722f3e95
Bhupen12 <68655036+Bhupen12@users.noreply.github.com>
2021-09-07 18:11:48 +0530

    Add files via upload

---
+++ src/components/TextForm.js
@@ -0,0 +1,56 @@
+import React , {useState} from 'react'
+
+
+export default function TextForm(props) {
+
+    const handleUpClick = () =>{
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase" , "success")
+    }
+
+    const handleLocClick = () =>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+
+    const handleOnChange = (event) =>{
+        setText(event.target.value)
+    }
+
+    const handleCopy = () =>{
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to clipboard" , "success")
+    }
+
+    const handleExtraSpace = () =>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const [text, setText] = useState('');
+    // setText("new Text");
+    return (
+        <>
+            <div className = "container" style = {{color : props.mode==='dark'?'white':'#051c3f'}}>
+                <h1 >{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value = {text} onChange={handleOnChange} id="myBox" rows="8" style = {{backgroundColor : props.mode==='dark'?'#112e39':'white' , color:props.mode==='dark'?'white':'#051c3f'}}></textarea>
+                </div>
+
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick = {handleUpClick}>Convert To UpperCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick = {handleLocClick}>Convert To LowerCase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick = {handleCopy}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-2" onClick = {handleExtraSpace}>handleExtraSpace</button>
+            </div>
+
+            <div className="container my-3" style = {{color : props.mode==='dark'?'white':'#051c3f'}}>
+                <h1>Your Text Summary</h1>
+                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words , {text.length} characters</p>
+                <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} minutes read time</p>
+                <h2>Preview</h2>
+                <p>{text.length > 0 ? text:"Nothing to preview"}</p>
+            </div>
+        </>
+    )
+}
